0c362f20cd08a1edb0e5b780a6f39d21d2101533
HITMANek210 <f.wyrembak@poczta.fm>
2023-08-28 15:17:58 +0200

    Added .env file

--- MyForum/settings.py
+++ MyForum/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x&t*rq8h3kh^=g%m46^%qi)&c6ex%*d9_hx_b_41a8*hmr_xs^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
